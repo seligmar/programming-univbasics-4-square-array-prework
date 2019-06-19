809f5fd36e08f3386f41c1ab03b143b1b143ce80
Mary Selig <seligmar@gmail.com>
2019-06-19 12:45:14 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
   counter = 0
   new_array = []
   while array < array.length 
-    array.each do {|num| num ** 2} 
+  #  array.each do {|num| num ** 2} 
    # new_array << num 
   counter += 1 
 end
